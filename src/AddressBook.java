3657fcffab29c32334563dce1856b7362f88bdcb
GolanHassin <golhassin@gmail.com>
2020-10-01 20:18:55 -0400

    git commit (third)

--- src/AddressBook.java
+++ src/AddressBook.java
@@ -6,18 +6,30 @@ public class AddressBook {
 
     public void addBuddy(BuddyInfo buddy)
     {
-        buddyInfo.add(buddy);
+        if(buddy != null)
+        {
+            buddyInfo.add(buddy);
+        }
     }
 
-    public void removeBuddy(BuddyInfo buddy)
+    public BuddyInfo removeBuddy(int index)
     {
-        buddyInfo.remove(buddy);
+        if(index >= 0 && index < buddyInfo.size())
+        {
+            return buddyInfo.remove(index);
+        }
+       return null;
     }
 
     public static void main(String[] args) {
         System.out.println("Address Book");
 
         BuddyInfo buddy = new BuddyInfo("Bob","123 Walker ST",1329800032);
+        AddressBook addressBook = new AddressBook();
+        addressBook.addBuddy(buddy);
+        addressBook.removeBuddy(0);
 
+        BuddyInfo bud = new BuddyInfo("Billy-Bob","321 Rawley Ave",230008932);
+        addressBook.addBuddy(bud);
     }
 }
---
+++ src/BuddyInfo.java
@@ -0,0 +1,21 @@
+public class BuddyInfo {
+
+    public BuddyInfo(String name, String address, int phoneNum)
+    {
+        this.name = name;
+        this.address = address;
+        this.phoneNum = phoneNum;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    private String name;
+
+    private String address;
+
+    private int phoneNum;
+
+
+}
